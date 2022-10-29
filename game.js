a09e1ebc2acb9032f04a3f6dab1eac9b025e8ada
dannyyyspam <danielaliabdulkarim@gmail.com>
2022-10-29 13:53:47 -0500

    add background image

--- game.js
+++ game.js
@@ -5,10 +5,52 @@ const ctx = cvs.getContext("2d");
 // GAME VARS AND CONSTS
 let frame = 0;
 
+// LOAD SPRITE IMAGE
+const sprite = new Image();
+sprite.src = "img/sprite.png";
+
+// BACKGROUND
+const bg = {
+  sX: 0,
+  sY: 0,
+  w: 275,
+  h: 226,
+  x: 0,
+  y: cvs.height - 226,
+
+  draw: function () {
+    ctx.drawImage(
+      sprite,
+      this.sX,
+      this.sY,
+      this.w,
+      this.h,
+      this.x,
+      this.y,
+      this.w,
+      this.h
+    );
+
+    ctx.drawImage(
+      sprite,
+      this.sX,
+      this.sY,
+      this.w,
+      this.h,
+      this.x + this.w,
+      this.y,
+      this.w,
+      this.h
+    );
+  },
+};
+
 // DRAW
 function draw() {
   ctx.fillStyle = "#70c5ce";
   ctx.fillRect(0, 0, cvs.width, cvs.height);
+
+  bg.draw();
 }
 
 // UPDATE
