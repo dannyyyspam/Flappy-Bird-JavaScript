53bd80f12c57c4ff4ee662af77c9606409bdc84c
dannyyyspam <danielaliabdulkarim@gmail.com>
2022-10-29 14:26:12 -0500

    add foreground image

--- game.js
+++ game.js
@@ -45,12 +45,48 @@ const bg = {
   },
 };
 
+// FOREGROUND
+const fg = {
+  sX: 276,
+  sY: 0,
+  w: 224,
+  h: 112,
+  x: 0,
+  y: cvs.height - 112,
+
+  draw: function () {
+    ctx.drawImage(
+      sprite,
+      this.sX,
+      this.sY,
+      this.w,
+      this.h,
+      this.x,
+      this.y,
+      this.w,
+      this.h
+    );
+    ctx.drawImage(
+      sprite,
+      this.sX,
+      this.sY,
+      this.w,
+      this.h,
+      this.x + this.w,
+      this.y,
+      this.w,
+      this.h
+    );
+  },
+};
+
 // DRAW
 function draw() {
   ctx.fillStyle = "#70c5ce";
   ctx.fillRect(0, 0, cvs.width, cvs.height);
 
   bg.draw();
+  fg.draw();
 }
 
 // UPDATE
