396e2cdf6c1d4ab3b2e8124afc40fbf3cae6e954
dannyyyspam <danielaliabdulkarim@gmail.com>
2022-10-29 14:32:39 -0500

    add bird

--- game.js
+++ game.js
@@ -80,6 +80,37 @@ const fg = {
   },
 };
 
+const bird = {
+  animation: [
+    { sX: 276, sY: 112 },
+    { sX: 276, sY: 139 },
+    { sX: 276, sY: 164 },
+    { sX: 276, sY: 139 },
+  ],
+  x: 50,
+  y: 150,
+  w: 34,
+  h: 26,
+
+  frame: 0,
+
+  draw: function () {
+    let bird = this.animation[this.frame];
+
+    ctx.drawImage(
+      sprite,
+      bird.sX,
+      bird.sY,
+      this.w,
+      this.h,
+      this.x + this.w,
+      this.y,
+      this.w,
+      this.h
+    );
+  },
+};
+
 // DRAW
 function draw() {
   ctx.fillStyle = "#70c5ce";
@@ -87,6 +118,7 @@ function draw() {
 
   bg.draw();
   fg.draw();
+  bird.draw();
 }
 
 // UPDATE
